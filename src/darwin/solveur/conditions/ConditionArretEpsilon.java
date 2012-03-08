cb4247a8771f5259d94d69fc88608ae5ef092d67
amaury.ollagnier <amaury.ollagnier@gmail.com>
2012-03-08 20:30:56 +0000

    Condition d'arrêt epsilon

---
+++ src/darwin/solveur/conditions/ConditionArretEpsilon.java
@@ -0,0 +1,90 @@
+ package darwin.solveur.conditions;
+
+import darwin.interfaces.IConditionArret;
+import darwin.interfaces.IEnvironnement;
+import darwin.interfaces.IPopulation;
+import darwin.modele.ConditionArret;
+
+/**
+ * Un condition d'arret Epsilon
+ * On arrête de boucler dans le cas ou la différence entre l'évaluation de la population courante
+ * et la dernière évaluée est inférieure à epsilon.* 
+ * 
+ * @author Amaury
+ *
+ */
+public class ConditionArretEpsilon extends ConditionArret{
+
+	// VARIALBES D'INSTANCES
+	
+	/**
+	 * Epsilon, la valeur de la condition d'arret epsilon
+	 */
+	private double epsilon;
+	
+	/**
+	 * La valeur de l'ancienne évaluation
+	 */
+	private double oldEvaluation;
+	
+	
+	
+	// CONSTRUCTEUR
+	
+	/**
+	 * Constructeur basic de la condition d'arrêt
+	 * @param epsilon, la valeur du esilon de la condition d'arrêt epsilon
+	 */
+	public ConditionArretEpsilon(double epsilon) {
+		this.epsilon = epsilon;
+		this.oldEvaluation = Double.MIN_VALUE;
+		
+	}
+	
+	public double getEpsilon() {
+		return epsilon;
+	}
+
+	public void setEpsilon(double epsilon) {
+		this.epsilon = epsilon;
+	}
+
+	@Override
+	public boolean isSatisfied(IPopulation population) {
+		if(oldEvaluation == Double.MIN_VALUE){
+			/* L'ancienne évaluation n'est pas encore instanciée */
+			oldEvaluation = population.evaluerPopulation();
+			return false;
+		}else{
+			double evaluationCourante = population.evaluerPopulation();					
+			if(Math.abs(oldEvaluation -  evaluationCourante) > this.epsilon){
+				/* La condition d'arrêt n'est pas satisfaite */
+				oldEvaluation = evaluationCourante;
+				return false;
+			}else{
+				oldEvaluation = evaluationCourante;
+				return true;
+			}
+		}
+
+	}
+
+	@Override
+	public IConditionArret nextConditionArret() {
+		return null;
+	}
+
+	@Override
+	public IEnvironnement nextEnvironnement() {
+		return null;
+	}
+
+	public double getOldEvaluation() {
+		return oldEvaluation;
+	}
+
+	public void setOldEvaluation(double oldEvaluation) {
+		this.oldEvaluation = oldEvaluation;
+	}
+
+}

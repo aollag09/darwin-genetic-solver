c797d973e105b8bdc4b99092300f5a3fb0ef458e
roy4490 <roy4490@gmail.com>
2012-03-10 13:59:04 +0000

    Rien de particulier, j'ai juste mit une boucle de tests dans "Résolution Tuples". J'ai testé l'obtiention d'un tuple de 10 entiers, sur 300 tests seulements 40 n'ont pas aboutis à la solution optimale (leur score était 1, c'est à dire qu'un seul entier différait de 1 de la solution). Le score moyen de la meilleure solution trouvée était 1.84, sachant que le score maximum est 2, je pense que c'est pas trop mal ! Sinon je finis la documentation aujourd'hui et je t'envoie ça !

--- src/tuples/ResolutionTuples.java
+++ src/tuples/ResolutionTuples.java
@@ -1,7 +1,10 @@
 package tuples;
 
+import java.util.ArrayList;
+
 import darwin.interfaces.IConditionArret;
 import darwin.interfaces.IEnvironnement;
+import darwin.interfaces.IIndividu;
 import darwin.interfaces.IPopulation;
 import darwin.interfaces.ISelectionNaturelle;
 import darwin.modele.ConditionArret;
@@ -11,52 +14,66 @@ import darwin.solveur.SelectionNaturelleSimple;
 public class ResolutionTuples {
 
 	public static void main(String[] args){
-		
-		IPopulation p = new PopulationTuples(20, 5, new int[] {1,2,4,0,15});
-		
-		try {
-			ISelectionNaturelle selec = new SelectionNaturelleSimple(p, 15, 1, 0.7, 1, 0.3, 10, 0.2);
+		ArrayList<Number> s = new ArrayList<Number>();
+		ArrayList<IIndividu> bests = new ArrayList<IIndividu>();
+		for(int i=0;i<300;i++){
+	
+			IPopulation p = new PopulationTuples(300, 10, new int[] {1,2,4,0,15,20,2,5,18,17});
 			
-			IConditionArret c = /*new ConditionArretEpsilon(0.05);*/new ConditionArret() {
+			try {
+				ISelectionNaturelle selec = new SelectionNaturelleSimple(p, 200, 1, 0.7, 1, 0.1, 150, 0.1);
 				
-				int c = 10;
-				double ancienScore = 0;
-				double epsilon = 0.05;
-				
-				@Override
-				public IEnvironnement nextEnvironnement() {
-					return null;
-				}
-				
-				@Override
-				public IConditionArret nextConditionArret() {
-					if(c>0){
-						c--;
-						return this;
-					}
-					else{
+				IConditionArret c = /*new ConditionArretEpsilon(0.05);*/new ConditionArret() {
+					
+					int c = 15;
+					double ancienScore = 0;
+					double epsilon = 0.05;
+					
+					@Override
+					public IEnvironnement nextEnvironnement() {
 						return null;
 					}
-				}
+					
+					@Override
+					public IConditionArret nextConditionArret() {
+						if(c>0){
+							c--;
+							return this;
+						}
+						else{
+							return null;
+						}
+					}
+					
+					@Override
+					public boolean isSatisfied(IPopulation population) {
+						this.iterations++;
+						double d = 	this.ancienScore - population.evaluerPopulation();
+						this.ancienScore = population.evaluerPopulation();
+						return (Math.abs(d)<this.epsilon);
+						//return (population.evaluerPopulation() > 100 || this.iterations>1000);
+					}
+				};
 				
-				@Override
-				public boolean isSatisfied(IPopulation population) {
-					this.iterations++;
-					double d = 	this.ancienScore - population.evaluerPopulation();
-					this.ancienScore = population.evaluerPopulation();
-					return (Math.abs(d)<this.epsilon);
-					//return (population.evaluerPopulation() > 100 || this.iterations>1000);
-				}
-			};
+				Darwin d = new Darwin(selec, c);
+				d.afficherChaqueScore(true);
+				IPopulation p1 = d.solve();
+				s.add(p1.evaluerIndividu(p1.getBestIndividu()));
+				bests.add(p1.getBestIndividu());
+				System.out.println("Meilleur : " + p1.getBestIndividu() + " (" + p1.evaluerIndividu(p1.getBestIndividu()) + ")");
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-			Darwin d = new Darwin(selec, c);
-			d.afficherChaqueScore(true);
-			d.afficherChaqueGeneration(true);
-			IPopulation p1 = d.solve();
-			System.out.println("Meilleur : " + p1.getBestIndividu() + " (" + p1.evaluerIndividu(p1.getBestIndividu()) + ")");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+		double moyenne = 0;
+		System.out.println("\n" + "\n" + " Meilleurs individus obtenus sur " + s.size() + " test :");
+		for(int i = 0; i<s.size(); i++){
+			moyenne += s.get(i).doubleValue();
+			System.out.println(bests.get(i) + " Score : " + s.get(i));
+		}
+		System.out.println("\n" + "Score moyen : " + moyenne/bests.size());
 	}
 }

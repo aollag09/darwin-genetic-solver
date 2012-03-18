71b87374b96b5e16a669698cd901ef74241055ff
amaury.ollagnier@gmail.com <amaury.ollagnier@gmail.com@f919a480-1c7e-5f7a-dc59-9febbeea90c3>
2012-03-18 14:27:33 +0000

    Ajout des classes pour la parralélisation

---
+++ trunk/src/parralelisation/client/MaitreTSP.java
@@ -0,0 +1,90 @@
+package parralelisation.client;
+
+import java.rmi.Naming;
+import java.util.ArrayList;
+
+import modele.genetique.tsp.EnvironnementTSP;
+import modele.genetique.tsp.PopulationTSP;
+import modele.genetique.tsp.SelectionNaturelleTSP;
+
+import darwin.interfaces.IConditionArret;
+import darwin.interfaces.IDarwin;
+import darwin.interfaces.IIndividu;
+import darwin.solveur.conditions.ConditionArretEpsilonAvecMarge;
+import darwin.solveur.crossovers.CrossOverChemin;
+import darwin.solveur.mutations.MutationChemin;
+import darwin.solveur.selections.SelectionElitiste;
+
+
+/**
+ * La classe Maitre TSP est la classe maitre de la parallélisation du problème du TSP.
+ * C'est lui qui va :
+ * 	> Initialiser les requêtes à envoyer sur tous les Serveurs disponnibles
+ * 	> Envoyez ces requêtes (une pour chaque serveur) avec ça liste de requêtes sur des threads différents
+ * 	> Récupérer les résulats des traitements des serveurs
+ * 	> Analyser ces résultats 
+ * @author Amaury
+ *
+ */
+public class MaitreTSP {
+	
+	public static void main(String[] args) {
+		MaitreTSP m = new MaitreTSP();
+		m.lancerRequetes();
+	}
+	
+	// CONSTANTES :
+	public static final String CHEMIN_RESEAU = "rmi://localhost:1099/Serveur";
+	
+	//VARIABLES D'INSTANCES : 
+	/** L'ensemble des meilleurs individus récupérés avec les générations sur les serveurs */
+	@SuppressWarnings("unused")
+	private ArrayList<IIndividu> bestIndividus;
+	/** Le nombre de serveurs disponnibles */
+	private int nombreServeurs;
+
+	public MaitreTSP() {
+		nombreServeurs = getNombreServeurs();
+		bestIndividus = new ArrayList<IIndividu>();
+	}
+	
+	public void lancerRequetes(){
+		/* On crée d'abord l'environement toujours indentique */
+		EnvironnementTSP environnement = new EnvironnementTSP();
+		/* Pour chacun des serveurs, on envoie une requête */
+		for(int i = 1; i <= nombreServeurs; i++){
+			try{
+			PopulationTSP population = new PopulationTSP(10, environnement);
+			SelectionNaturelleTSP stsp = new SelectionNaturelleTSP(new SelectionElitiste(10), 
+					new SelectionElitiste(10), new CrossOverChemin(0.8), new MutationChemin(0.9),population,10,1);
+			IConditionArret condition = new ConditionArretEpsilonAvecMarge(0.01, 500);
+			Requete r = new Requete(i, this, stsp, condition);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	
+	public static int getNombreServeurs(){
+		boolean erreur = false;
+		int indexServeur = 0;
+		while( ! erreur ){
+			indexServeur ++;
+			try { 
+				String adresse = CHEMIN_RESEAU+indexServeur;
+				IDarwin darwin = (IDarwin)Naming.lookup(adresse); 
+			}
+			catch (Exception e) { 
+				erreur = true;
+			} 
+		}
+		return indexServeur;
+	}
+	
+	
+
+}
---
+++ trunk/src/parralelisation/serveur/ServeurTSP.java
@@ -0,0 +1,74 @@
+package parralelisation.serveur;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+import parralelisation.client.MaitreTSP;
+
+
+import darwin.interfaces.IDarwin;
+import darwin.solveur.Darwin;
+
+/**
+ * 
+ * Cette classe permet de créer un serveur sur lequel on peut donner différentes commandes concernant 
+ * l'algorithme génétique du TSP.
+ * 
+ * 
+ * 
+ * @author Amaury
+ *
+ */
+public class ServeurTSP {
+
+	public ServeurTSP(){
+		boolean erreur = false;
+		String adresse = "";
+		/* Chaque serveur doit créer un objet Darwin sur lequel il va fair tourner toutes les mutations ! */
+		IDarwin darwin;
+		try {
+			darwin = new Darwin();
+			
+			
+			/* On cherche à déposer cet objet sur le réseau, en trouvant le dernier disponnible */
+			int bonIndex = 0;
+			int index = 1;
+			while(bonIndex == 0){
+				try { 
+					@SuppressWarnings("unused")
+					IDarwin d = (IDarwin)Naming.lookup(
+									MaitreTSP.CHEMIN_RESEAU+index); 
+				}catch (Exception e) {
+					bonIndex = index;
+				}
+				index ++;
+			}
+			
+			/* On dépose alors sur le serveur l'objet Darwin avec la bonne adresse ! */
+			adresse = MaitreTSP.CHEMIN_RESEAU+bonIndex;
+			
+			
+			Naming.rebind(adresse, darwin);
+			
+		} catch (RemoteException e1) {
+			erreur = true;
+			e1.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			erreur = true;
+		}
+		
+	
+		if( ! erreur){
+			System.out.println("Serveur lancé sur : "+adresse);
+			System.out.println("En attente de requête ...");
+		}else
+			System.out.println("Assurez vous d'avoir bien lancé le rmiregistry dans le dossier bin de Darwin avant de créer un Serveur");
+		/* Le serveur est près à traiter vos requêtes */
+	}
+	
+	public static void main(String[] args) {
+		new ServeurTSP();
+	}
+}
